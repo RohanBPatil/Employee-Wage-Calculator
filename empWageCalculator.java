c7e4d6048175ed406261426e9222c386e5e9d141
RohanBPatil <rohanpatil14218@gmail.com>
2020-09-27 14:45:59 +0530

    uc1 Attendance Feature

--- empWageCalculator.java
+++ empWageCalculator.java
@@ -3,5 +3,13 @@ package empWageCalculator;
 public class empWageCalculator {
 	public static void main(String[] args) {
 		System.out.println("Welcome To Employee Wage Calculator");
+		
+		int is_full_time = 1;
+		double empCheck = Math.floor(Math.random() * 10) % 2;
+		
+		if (empCheck == is_full_time)
+			System.out.println("Employee is present");
+		else
+			System.out.println("Employee is absent");
 	}
 }
