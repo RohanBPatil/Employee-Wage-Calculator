66575c5495a7b2cf45e151003b8fe4224d3e4808
RohanBPatil <rohanpatil14218@gmail.com>
2020-09-27 15:16:59 +0530

    Added part time employee and Wage

--- empWageCalculator.java
+++ empWageCalculator.java
@@ -5,25 +5,26 @@ public class empWageCalculator {
 		System.out.println("Welcome To Employee Wage Calculator");
 		
 		//constants
-				int is_full_time = 1;
+				int is_part_time = 1;
+				int is_full_time = 2;
 				int emp_wage_per_hr = 20;
 				//variables
 				int empHrs = 0;
 				int empWage = 0;
-				double empCheck = Math.floor(Math.random() * 10) % 2;
+				double empCheck = Math.floor(Math.random() * 10) % 3;
 				
 				if (empCheck == is_full_time) {
-					System.out.println("Employee is present");
+					empHrs = 8;
+				}
+				else if (empCheck == is_part_time) {
 					empHrs = 8;
 				}
 				else {
-					System.out.println("Employee is absent");
 					empHrs = 0;
 				}
 				
 				//computation
 				empWage = emp_wage_per_hr * empHrs;
 				System.out.println("Employee Wage : "+empWage);
-				
 	}
 }
