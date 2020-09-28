36e11248aea54ffca36e7bdc04e1165184d54383
RohanBPatil <rohanpatil14218@gmail.com>
2020-09-28 16:26:06 +0530

    Refactored : EmpWageBuilder to manage wage for multiple companies using ARRAY and created CompanyEmpWage class

---
+++ CompanyEmpWage.java
@@ -0,0 +1,24 @@
+package empWageCalculator;
+
+public class CompanyEmpWage {
+	public final String company;
+	public final int emp_wage_per_hr;
+	public final int max_working_days;
+	public final int max_hrs_in_month;
+	public int total_empWage;
+	
+	public CompanyEmpWage(String company, int emp_wage_per_hr, int max_working_days, int max_hrs_in_month) {
+		this.company = company;
+		this.emp_wage_per_hr = emp_wage_per_hr;
+		this.max_working_days = max_working_days;
+		this.max_hrs_in_month = max_hrs_in_month;
+	}
+	
+	public void setTotalEmpWage(int total_empWage) {
+		this.total_empWage = total_empWage;
+	}
+	
+	public String toString() {
+		return "Total Emp Wage for company '"+company+"' is : "+total_empWage;
+	}
+}
---
+++ EmpWageBuilder.java
@@ -0,0 +1,71 @@
+package empWageCalculator;
+
+public class EmpWageBuilder {
+	public static final int is_part_time = 1;
+	public static final int is_full_time = 2;
+	
+	private int numOfCompany = 0;
+	private CompanyEmpWage[] companyEmpWageArray;
+	
+	
+	public EmpWageBuilder() {
+		companyEmpWageArray = new CompanyEmpWage[5];
+	}
+	
+	private void addCompanyEmpWage(String company, int emp_wage_per_hr, int max_working_days, int max_hrs_in_month) {
+		companyEmpWageArray[numOfCompany] = new CompanyEmpWage(company, emp_wage_per_hr, max_working_days, max_hrs_in_month);
+		numOfCompany++;
+	}
+	
+	private void computeEmpWage() {
+		for(int i = 0; i < numOfCompany; i++) {
+			companyEmpWageArray[i].setTotalEmpWage(calculateEmpWage(companyEmpWageArray[i]));
+			System.out.println(companyEmpWageArray[i]);
+		}
+	}
+	//Method to calculate employee wage
+	private int calculateEmpWage(CompanyEmpWage com) {
+		//variables
+		int empHrs = 0;
+		int total_empHrs = 0;
+		int total_working_days = 0;
+		int total_empWage = 0;
+		
+		//computation
+		while(total_empHrs <= com.max_hrs_in_month && total_working_days < com.max_working_days) {
+			total_working_days++;
+			int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+			switch(empCheck) {
+			case is_part_time:
+				empHrs = 8;
+				break;
+			case is_full_time:
+				empHrs = 8;
+				break;
+			default:
+				empHrs = 0;
+				break;
+			}
+			total_empHrs += empHrs;
+			System.out.println("Day : "+total_working_days+" Emp Hrs : "+empHrs );
+		}
+		
+		/*
+		if(total_empHrs >= max_hrs_in_month)
+			total_empHrs = max_hrs_in_month;															//if total hrs exceeds max hrs per month then computing for max hrs per month
+		*/
+		
+		total_empWage = total_empHrs * com.emp_wage_per_hr;
+		return total_empWage;
+	}
+	
+	
+	public static void main(String[] args) {
+		System.out.println("Welcome To Employee Wage Calculator");
+		
+		EmpWageBuilder ewb = new EmpWageBuilder();
+		ewb.addCompanyEmpWage("DMart", 20, 2, 10);
+		ewb.addCompanyEmpWage("Reliance", 10, 4, 20);
+		ewb.computeEmpWage();	
+	}
+}
