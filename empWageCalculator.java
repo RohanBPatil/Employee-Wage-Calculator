b27712c4da0f759f31b682f15b27a0d93a4ac6d0
RohanBPatil <rohanpatil14218@gmail.com>
2020-09-28 13:19:14 +0530

    Added : Functionality to calculate wage for multiple companies

--- empWageCalculator.java
+++ empWageCalculator.java
@@ -2,11 +2,11 @@ package empWageCalculator;
 
 public class empWageCalculator {
 	//Global constants
-	static final int is_part_time = 1;
-	static final int is_full_time = 2;
+	public static final int is_part_time = 1;
+	public static final int is_full_time = 2;
 	
 	//Method to calculate employee wage
-	public static int calculateEmpWage(int emp_wage_per_hr,int max_working_days,int max_hrs_in_month) {
+	public static int calculateEmpWage(String company, int emp_wage_per_hr,int max_working_days,int max_hrs_in_month) {
 		//variables
 		int empHrs = 0;
 		int total_empHrs = 0;
@@ -32,10 +32,15 @@ public class empWageCalculator {
 			total_empHrs += empHrs;
 			System.out.println("Day : "+total_working_days+" Emp Hrs : "+empHrs );
 		}
+		
+		/*
 		if(total_empHrs >= max_hrs_in_month)
 			total_empHrs = max_hrs_in_month;															//if total hrs exceeds max hrs per month then computing for max hrs per month
+		*/
 		
+		System.out.println("Total emp hrs : "+total_empHrs);
 		total_empWage = total_empHrs * emp_wage_per_hr;
+		System.out.println("Total Emp wage for Company '"+company+"' is : "+total_empWage);
 				
 		return total_empWage;
 	}
@@ -43,16 +48,8 @@ public class empWageCalculator {
 	public static void main(String[] args) {
 		System.out.println("Welcome To Employee Wage Calculator");
 		
-		//constants
-		int emp_wage_per_hr = 20;
-		int max_working_days = 20;
-		int max_hrs_in_month = 100;
-		//variables
-		int total_empWage = 0;
-		
-		//computation
-		total_empWage = calculateEmpWage(emp_wage_per_hr, max_working_days, max_hrs_in_month);
-		System.out.println("Total Employee Wage : "+total_empWage);
+		calculateEmpWage("DMart", 20, 2, 10);
+		calculateEmpWage("Reliance", 10, 4, 20);
 				
 	}
 }
